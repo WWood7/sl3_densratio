1bd36050d52820c42e9f3f2c10223e5cc1b90a33
Wencheng Wu <menacewood7@gmail.com>
2023-10-29 23:19:36 -0400

    Update Lrnr_densratio_classification.R
    Modify the classification learner, allow it to do a 2-stage estimation.

--- R/Lrnr_densratio_classification.R
+++ R/Lrnr_densratio_classification.R
@@ -40,12 +40,15 @@ Lrnr_densratio_classification <- R6Class(
     classname = "Lrnr_densratio_classification", inherit = Lrnr_base,
     portable = TRUE, class = TRUE,
     public = list(
-        initialize = function(classifier = NULL, ...) {
+        initialize = function(classifier = NULL, ub = NULL, stage2 = FALSE,...) {
             # this captures all parameters to initialize and saves them as self$params
             params <- args_to_list()
             
             if (is.null(params$classifier)) {
                 params$classifier <- make_learner(Lrnr_glm_fast)
+            }
+            if (is.null(params$ub)){
+                params$ub <- 10
             }
             super$initialize(params = params, ...)
         },
@@ -61,10 +64,16 @@ Lrnr_densratio_classification <- R6Class(
         .properties = c("densratio"),
         # .train takes task data and returns a fit object that can be used to generate predictions
         .train = function(task) {
+            if (self$params$stage2 == TRUE){
+                # modify the task
+                covariates_set <- task$nodes$covariates[2:length(task$nodes$covariates)]
+                task <- task$next_in_chain(
+                    covariates = covariates_set
+                )
+            }
             classifier <- self$params$classifier
             prob_fit <- classifier$train(task)
             fit_object <- prob_fit
-            
             # return the fit object, which will be stored
             # in a learner object and returned from the call
             # to learner$predict
@@ -73,9 +82,41 @@ Lrnr_densratio_classification <- R6Class(
         
         # .predict takes a task and returns predictions from that task
         .predict = function(task = NULL) {
+            # check stage
+            if (self$params$stage2 == TRUE){
+                stage1_results <- task$data$stage1_results
+                covariates_set <- task$nodes$covariates[2:length(task$nodes$covariates)]
+                task <- task$next_in_chain(
+                    covariates = covariates_set
+                )
+            }
+            # get the upper bound
+            ub <- self$params$ub
+          
+            
             prob <- self$fit_object$predict(task)
             predictions <- prob / (1 - prob)
+            # if this is the stage2 estimation    
+            if (self$params$stage2 == TRUE){
+                predictions <- stage1_results / predictions
+            }
+            # apply the upper bound
+            predictions <- pmin(predictions, ub)
             return(predictions)
+        },
+        
+        # ddd
+        .chain = function(task){
+            # the second to the last are the sets of variables we condition on
+            stage1_results <- self$predict(task)
+            stage1_results <- as.data.table(stage1_results)
+            task <- task$revere_fold_task("full")
+            new_col_names <- task$add_columns(stage1_results, self$fit_uuid)
+            covariates_set <- c(names(stage1_results), task$nodes$covariates[2:length(task$nodes$covariates)])
+            return(task$next_in_chain(
+                covariates = covariates_set,
+                column_names = new_col_names
+            ))
         }
     )
 )
